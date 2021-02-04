ca79ff019c2045d021a81df938c73558acecbf5e
Bryan Hilldrup <hilldrupbf@vcu.edu>
2021-02-04 15:29:45 -0500

    Getting array of results from input array of track ids

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -6,21 +6,24 @@
 const fs = require('fs');
 const credentials = require('../../spotifyCredentials.json');
 const request = require('request');
-const querystring = require('querystring');
 const axios = require('axios');
+// Query string needed so payload is in correct format
+const querystring = require('querystring');
 
 // Checks to see if last auth token was fetched more than 50 minutes ago
 // Uses refresh token to get new auth token and updates time stamp
-if (credentials.lastTimeStamp >= Date.now() - 3000000) {
+if (credentials.lastTimeStamp <= Date.now() - 3000000) {
   getNewAuthToken();
 }
 
 function getNewAuthToken() {
   let url = 'https://accounts.spotify.com/api/token';
+
   let data = querystring.stringify({
     grant_type: 'refresh_token',
     refresh_token: credentials.refresh_token,
   });
+
   let config = {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -38,11 +41,47 @@ function getNewAuthToken() {
   });
 }
 
-// async function getSingleTracks(trackIds) {
-//   trackIds.forEach(id => {
-//     // Query spotify for track
-//     const track = await app.get(`https://api.spotify.com/v1/tracks/${id}`);
-//   });
-// }
+function getSingleTracks(trackIds) {
+  let config = {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Bearer ' + credentials.authorization_code,
+    },
+  };
 
-function getTracksFromAlbums() {}
+  Promise.all(
+    trackIds.map(
+      async id =>
+        await axios.get(`https://api.spotify.com/v1/tracks/${id}`, config)
+    )
+  )
+    .then(results => results.forEach(result => console.log(result)))
+    .catch(err => console.log(err));
+}
+
+// TESTING
+const testTracks = ['2G90KzHn8ynh7Ai48hPJoR', '5OkLfBehmMPQXnCK9tNRd8'];
+getSingleTracks(testTracks);
+
+// function getTracksFromAlbums() {}
+
+// Tracks
+//-------------
+// Name:   res.data.name
+// Artist: res.data.artists[0].name  --> One index per artist
+// Album:  res.data.album.album_type === 'album', res.data.album.name
+// Album:  res.data.album.album_type === 'single', single
+// Length: res.data.duration_ms --> Convert to min:sec
+
+// Albums
+//----------
+// Some albums have only one song
+// Some track links are linked by album
+// album_type can be labeled 'single'
+// Get all album links with one song and add them to tracks playlist
+// If album link, res.data.type === 'album', res.data.total_tracks >= 1, get res.data.tracks.items[]
+
+// Playlists
+//-------------
+
+// getTracks() {}
