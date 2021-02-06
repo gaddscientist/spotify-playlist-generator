31dc126f226a1d0b6d73805ad85c38449104f79a
Bryan Hilldrup <hilldrupbf@vcu.edu>
2021-02-06 14:07:57 -0500

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,24 +1,38 @@
-# spotify-playlist-generator
+# Spotify Playlist Generator
+A single page web application built with Vue 3(as well as Vuex and Vue Router) to create Spotify playlists from Reddit posts.
+
+## *Features*
+    -User can grab posts from a multireddit or subreddit
+    -User can sort by Hot, Top, New, or Rising posts
+    -User can choose which time period to grab Top posts from(Day, Week, Month, Year, All Time)
+    -User can optionally include album or playlists posted to Reddit
+
+    -Uses both Spotify and Reddit's public API's
+
+## *Coming Soon*
+    -Deploying to public URL
+    -Allowing user to export playlist to their own Spotify account
+    -Allowing user to edit playlist in browser before exporting(removing songs, reordering, etc)
+    -Node.js back-end to support simple API
+    -Authentication to allow user to login and save created playlists
+    -Other websites to grab songs from besides Reddit
+
+### *Creation Form*
+<kbd>
+  <img src="https://github.com/hilldrupbf/spotify-playlist-generator/blob/master/img/form.png" width="800">
+</kbd>
+
+### *Playlist*
+<kbd>
+  <img src="https://github.com/hilldrupbf/spotify-playlist-generator/blob/master/img/playlist.png" width="800">
+</kbd>
 
 ## Project setup
 ```
 npm install
 ```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
 ### Compiles and minifies for production
 ```
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/PlaylistPage.vue
+++ src/views/PlaylistPage.vue
@@ -34,41 +34,65 @@ export default {
   components: {
     BaseCard,
   },
-  props: ['tracks'],
+  // props: ['tracks'],
   data() {
     return {
-      // tracks: [
-      //   {
-      //     title: 'Work',
-      //     artist: 'Gang Starr',
-      //     album: 'Moment Of Truth',
-      //     duration: '2:58',
-      //   },
-      //   {
-      //     title: 'Deadly Combination',
-      //     artist: 'Big L, Tupac',
-      //     album: 'The Big Picture',
-      //     duration: '2:32',
-      //   },
-      //   {
-      //     title: 'Still',
-      //     artist: 'Geto Boys',
-      //     album: 'The Resurrection',
-      //     duration: '4:00',
-      //   },
-      //   {
-      //     title: 'Drop',
-      //     artist: 'The Pharcyde',
-      //     album: 'Labcabincalifornia',
-      //     duration: '5:34',
-      //   },
-      //   {
-      //     title: 'Grown Man Sport',
-      //     artist: 'Pete Rock, Inl',
-      //     album: 'Center of Attention',
-      //     duration: '4:36',
-      //   },
-      // ],
+      tracks: [
+        {
+          title: 'Work',
+          artist: 'Gang Starr',
+          album: 'Moment Of Truth',
+          duration: '2:58',
+        },
+        {
+          title: 'Deadly Combination',
+          artist: 'Big L, Tupac',
+          album: 'The Big Picture',
+          duration: '2:32',
+        },
+        {
+          title: 'Still',
+          artist: 'Geto Boys',
+          album: 'The Resurrection',
+          duration: '4:00',
+        },
+        {
+          title: 'Drop',
+          artist: 'The Pharcyde',
+          album: 'Labcabincalifornia',
+          duration: '5:34',
+        },
+        {
+          title: 'Grown Man Sport',
+          artist: 'Pete Rock, Inl',
+          album: 'Center of Attention',
+          duration: '4:36',
+        },
+        {
+          title: 'There He Go',
+          artist: 'ScHoolboy Q',
+          album: 'Habits & Contradictions',
+          duration: '3:20',
+        },
+        {
+          title: 'The Light',
+          artist: 'Common',
+          album: 'Go! Common Classics',
+          duration: '4:04',
+        },
+        {
+          title: 'Survival Tactics',
+          artist: 'Joey Bada$$, Capital Steez',
+          album: '1999',
+          duration: '3:23',
+        },
+        {
+          title: 'Cherry WIne',
+          artist: 'Nas, Amy Winehouse',
+          album: 'Life Is Good(Deluxe)',
+          duration: '5:56',
+        },
+      ],
     };
   },
 };
@@ -92,7 +116,7 @@ export default {
   min-height: 40px;
   padding: 5px;
   color: #fff;
-  font-size: 13px;
+  font-size: 15px;
   border-bottom: 1px solid #333;
 }
 .column-header-item,
